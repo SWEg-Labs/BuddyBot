f9a3e24371df7ca9b2f8eb21d90c8814b2d04ef3
rickyz03 <riccardost02@gmail.com>
2024-12-15 18:42:59 +0100

    Rimossa ridondanza nella descrizione di logger

--- src/backend/utils/logger.py
+++ src/backend/utils/logger.py
@@ -3,8 +3,6 @@ import os
 # Ottiene la configurazione LOGGING_ENABLED dalle variabili di ambiente
 LOGGING_ENABLED = os.getenv("LOGGING_ENABLED", "false").lower() == "true"
 
-# Utility logger che può essere attivato/disattivato tramite variabili di ambiente
-# Wrappa print con formattazione e controllo aggiuntivi
 class Logger:
     """
     Utility logger that can be enabled/disabled via environment variables.
