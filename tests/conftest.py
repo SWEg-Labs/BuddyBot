c5ff7612e9273b9ef50b703bcd60944bdce7c454
rickyz03 <riccardost02@gmail.com>
2025-02-24 01:12:23 +0100

    Piccolo cambio di ordine

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-# Abilita pytest-asyncio per eseguire test asincroni
-pytest_plugins = "pytest_asyncio"
-
 # Imposta il percorso per importare i moduli dal backend
 current_dir = os.path.dirname(os.path.abspath(__file__))
 src_path = os.path.abspath(os.path.join(current_dir, "..", "src", "backend"))
 sys.path.insert(0, src_path)
+
+# Abilita pytest-asyncio per eseguire test asincroni
+pytest_plugins = "pytest_asyncio"
