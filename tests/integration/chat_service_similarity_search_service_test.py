08ab817a5017862e600fa0f68abcd3f9ebbac0ae
rickyz03 <riccardost02@gmail.com>
2025-02-27 23:11:00 +0100

    [BUD-321] Cambio di approccio per i test delle classi Facade: ha senso testare solamente i singoli metodi che fanno la chiamata verso un servizio solo, non ha senso creare tutti i tipi di dati 'mock' intermedi per un test solo del metodo gigante pubblico

--- tests/integration/chat_service_similarity_search_service_test.py
+++ tests/integration/chat_service_similarity_search_service_test.py
@@ -14,18 +14,13 @@ def test_chat_service_calls_similarity_search_service():
     mock_generate_answer_service = MagicMock(spec=GenerateAnswerService)
     chat_service = ChatService(mock_similarity_search_service, mock_generate_answer_service)
     user_input = Question("test query")
-    documents = [Document(page_content="Test content", metadata={"distance": 0.5})]
-    mock_similarity_search_service.similarity_search.return_value = documents
-    mock_generate_answer_service.generate_answer.return_value = Answer("Test answer")
+    relevant_docs = [Document(page_content="Test content", metadata={"distance": 0.5})]
+    mock_similarity_search_service.similarity_search.return_value = relevant_docs
     
     # Act
-    chat_service.get_answer(user_input)
+    result = chat_service.similarity_search(user_input)
     
     # Assert
     mock_similarity_search_service.similarity_search.assert_called_once_with(user_input)
-
-    # Non si può testare l'output perchè esso l'output della similarity_search viene passato al generate_answer_service,
-    # e poi non viene restituito direttamente ma viene usato per creare un oggetto Answer che viene restituito.
-    # Quindi non si può testare l'"output coincidente", bensì si può testare solo la "chiamata con il giusto input".
-    # assert result == mock_documents
+    assert result == relevant_docs
 
---
+++ tests/integration/load_files_service_load_files_port.py
@@ -0,0 +1,44 @@
+from unittest.mock import MagicMock
+from models.document import Document
+from services.loadFilesService import LoadFilesService
+from ports.githubPort import GitHubPort
+from ports.jiraPort import JiraPort
+from ports.confluencePort import ConfluencePort
+from ports.loadFilesPort import LoadFilesPort
+from ports.saveLoadingAttemptInDbPort import SaveLoadingAttemptInDbPort
+from datetime import datetime
+from models.vectorStoreLog import VectorStoreLog
+
+# Verifica che il metodo load_in_vector_store di LoadFilesService chiami il metodo load di LoadFilesPort
+
+def test_load_calls_port_method():
+    # Arrange
+    mock_github_port = MagicMock(spec=GitHubPort)
+    mock_jira_port = MagicMock(spec=JiraPort)
+    mock_confluence_port = MagicMock(spec=ConfluencePort)
+    mock_load_files_port = MagicMock(spec=LoadFilesPort)
+    mock_save_loading_attempt_in_db_port = MagicMock(spec=SaveLoadingAttemptInDbPort)
+    load_files_service = LoadFilesService(
+        mock_github_port, mock_jira_port,
+        mock_confluence_port, mock_load_files_port,
+        mock_save_loading_attempt_in_db_port,
+    )
+    documents = [
+        Document(page_content="doc1", metadata={"type": "pdf"}),
+        Document(page_content="doc2", metadata={"type": "docx"}),
+    ]
+    vector_store_log = VectorStoreLog(
+        timestamp=datetime.now(),
+        outcome=True,
+        num_added_files=2,
+        num_modifed_files=0,
+        num_deleted_files=0
+    )
+    mock_load_files_port.load.return_value = vector_store_log
+
+    # Act
+    result = load_files_service.load_in_vector_store(documents)
+
+    # Assert
+    mock_load_files_port.load.assert_called_once_with(documents)
+    assert result == "mocked_vector_store_log"
