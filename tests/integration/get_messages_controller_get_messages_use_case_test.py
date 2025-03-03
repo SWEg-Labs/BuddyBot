938f666d4521d0b01501b58a776a053aa51b5649
rickyz03 <riccardost02@gmail.com>
2025-03-03 12:39:29 +0100

    Droppati i test del ramo SaveMessageService

---
+++ tests/integration/get_messages_controller_get_messages_use_case_test.py
@@ -0,0 +1,34 @@
+from unittest.mock import MagicMock
+
+from dto.messageBaseModel import MessageBaseModel, MessageSenderBaseModel
+from models.quantity import Quantity
+from models.message import Message, MessageSender
+from controllers.getMessagesController import GetMessagesController
+from use_cases.getMessagesUseCase import GetMessagesUseCase
+
+# Verifica che il metodo get_messages di GetMessagesController chiami il metodo get_messages di GetMessagesUseCase
+
+def test_get_messages_calls_use_case_method():
+    # Arrange
+    mock_get_messages_use_case = MagicMock(spec=GetMessagesUseCase)
+    get_messages_controller = GetMessagesController(mock_get_messages_use_case)
+
+    quantity = 5
+    quantity_dict = {"value": quantity}
+    quantity_object = Quantity(quantity)
+    expected_result = [
+        MessageBaseModel(content=f"Message {i}", timestamp=f"2021-10-10T10:10:0{i}",
+                                        sender=MessageSenderBaseModel.USER) for i in range(quantity)
+    ]
+    use_case_result = [
+        Message(content=f"Message {i}", timestamp=f"2021-10-10T10:10:0{i}",
+                               sender=MessageSender.USER) for i in range(quantity)
+    ]
+    mock_get_messages_use_case.get_messages.return_value = use_case_result
+
+    # Act
+    result = get_messages_controller.get_messages(quantity_dict)
+
+    # Assert
+    mock_get_messages_use_case.get_messages.assert_called_once_with(quantity_object)
+    assert result == expected_result
