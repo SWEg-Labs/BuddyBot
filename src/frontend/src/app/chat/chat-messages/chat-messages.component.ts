ea00633d30eebce651a72da63d278cc769f0a771
rickyz03 <riccardost02@gmail.com>
2025-03-17 01:13:13 +0100

    Ho messo in rosso il messaggio di errore del chatbot

--- src/frontend/src/app/chat/chat-messages/chat-messages.component.ts
+++ src/frontend/src/app/chat/chat-messages/chat-messages.component.ts
@@ -16,6 +16,7 @@ export class ChatMessagesComponent implements AfterViewInit {
   @Input() isLoading = false
   @Input() loadingOlderMessages = false
   @Input() showScrollToBottom = false
+  @Input() errorMessage = ''
   @Output() isScrolledUp = new EventEmitter<boolean>()
   @Output() loadMoreMessages = new EventEmitter<void>()
   @ViewChild('scrollMe') private readonly messagesContainer!: ElementRef
